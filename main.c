2dccb0cef12579f63a8998c4395b4d38db7efd92
Vynicius <leandro.vynicius123@gmail.com>
2022-09-25 21:12:58 -0300

    Inicializando janela

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "raylib.h"
+
+int main(void)
+{
+ 
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);           
+  
+
+    
+    while (!WindowShouldClose())   
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Agora falta o resto", 200, 200, 20, BLUE);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
